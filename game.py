025f18a89868f793dec387e91f2ea3e8e4588372
Felix <felixgan@gmail.com>
2020-11-30 16:17:57 +0800

    adjusted simulationto actually have reasonable iterations (previously 1 iterations already removed all edges)

--- game.py
+++ game.py
@@ -15,38 +15,34 @@ def initalize(nodes, adj, num_nodes):
                         adj[i][j] = edge_type
                         adj[j][i] = edge_type
 
-                
-
 def simulation(nodes, a, num_iterations):
+        random.seed()
         for _ in range (num_iterations):
                 for i in range (0, num_nodes):
                         for j in range (0, i):
                                 if adj[i][j]:
                                         if nodes[i] == 1 or nodes[j] == 1:
-                                                adj[i][j] = 0
-                                                adj[j][i] = 0
+                                                if (random.randint(0, 100) < 10):      # 10 percent chance
+                                                        adj[i][j] = 0
+                                                        adj[j][i] = 0
                                         else:
                                                 adj[i][j] = 1
                                                 adj[j][i] = 1
 
 def visualization(nodes, adj):
         G = nx.convert_matrix.from_numpy_matrix(adj)
-        nodes_color = []
-
         color = ['#03b500', '#b52a00']         # green, red (orangish)
-        for i in range (len(nodes)):
-                if nodes[i] == 0:
-                        nodes_color.append(color[0])    # green = cooperate
-                else:
-                        nodes_color.append(color[1])    # red = defect
-        
-        nx.draw(G, with_labels=True, node_color = nodes_color)
+        nodes_color = [color[1] if (nodes[i]) else color[0] for i in range (len(nodes))]
+
+        optimized_graph = nx.spring_layout(G)
+        plt.figure(figsize = (10, 10))  # Make figure bigger so we can actually see all the edges! (See what happens otherwise by calling nx.draw(G) before this line.)
+        nx.draw(G, optimized_graph, with_labels=False, node_color = nodes_color, node_size = 15, width=0.15)
         plt.show()
 
 if __name__ == '__main__':
         #  defining variables
         num_nodes = 100
-        num_iterations = 100
+        num_iterations = 20
         nodes = {}
         adj = np.zeros((num_nodes, num_nodes))
 
