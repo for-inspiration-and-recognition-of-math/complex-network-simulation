569f2ab0c4a913a91c854e4b2672c627adaddf1b
Minglu Lucy Zhao <lucyzhao1999@gmail.com>
2020-12-04 10:51:31 -0800

    changed simulations to having no multi-edge, fixed list appending problem

--- game_main.py
+++ game_main.py
@@ -1,30 +1,38 @@
-import networkx as nx
-import matplotlib.pyplot as plt
 from simulations import *
 from cleaning import *
 from visualization import *
 
 if __name__ == '__main__':
-        num_nodes = 50
-        numIterations = 100
+        num_nodes = 10
+        numIterations = 20
         
-        # gneerate network
+        # generate network
         nodes, adj = generate_random_network("ER", num_nodes, p=0.8)
         
 
         
         # run simulation
         saveRate = 1
-        simulation_simplest = Simulation(numIterations, saveRate, strategy= 2) # choose from 0, 1, 2
+        simulation_simplest = Simulation(numIterations, saveRate, strategy= 1) # choose from 0, 1, 2
         
         
         nodes_list, adj_list = simulation_simplest(nodes, adj)
 
+        print(adj_list[0])
+        print(adj_list[1])
+        print(adj_list[-1])
+        print([[node.wealth for node in nodes.values()] for nodes in nodes_list])
+
+        print([[node.status for node in nodes.values()] for nodes in nodes_list])
+
+        #
+
+
         
         # visualize
         
-        visualize_list(nodes_list, adj_list, numIterations)
-        generate_gif()
+        # visualize_list(nodes_list, adj_list, numIterations)
+        # generate_gif()
 
         
 
