4c4bcdfac6cc3780afcfbd90fac6ef2601de8a56
Felix <felixgan@gmail.com>
2020-12-11 01:10:06 +0800

    another graph functionality (per iteration gain VS nodes) + added more threads

--- game_main.py
+++ game_main.py
@@ -17,7 +17,7 @@ def complexity_graph():
                 num_nodes = i
                 numIterations = 1
                 
-                # gneerate network
+                # generate network
                 nodes, adj = generate_random_network("BA", num_nodes, p=0.9)
                 # nodes, adj = facebook_clean()
 
@@ -69,34 +69,65 @@ def complexity_graph():
         plt.show()             
         plt.close()
 
+
+def load_data(strat, payoff, saveRate):
+        start = time.perf_counter()
+        
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        
+        fd = open(f'{dir_path}/savedInfo/adjMat_strategy{strat}payoff{payoff}saveRate{saveRate}.pickle', 'rb')
+        adj_list = pickle.load(fd)
+
+        fd = open(f'{dir_path}/savedInfo/nodesDict_strategy{strat}payoff{payoff}saveRate{saveRate}.pickle', 'rb')
+        nodes_list = pickle.load(fd)
+        
+        numIterations = len(nodes_list)-1       # -1 since it included the initial, uniterated graph as well
+        
+        print(f'time used to unpickle : {time.perf_counter()-start}s')
+        
+        return nodes_list, adj_list, numIterations
+
+
 if __name__ == '__main__':
         start = time.perf_counter()
         
-        num_nodes = 100
-        numIterations = 200
-        model = "ER"
+        num_nodes = int(input("Node # (default: 200): ") or "200")
+        numIterations = int(input("Iteration # (default: 100): ",) or "100")
+        model = str(input("Model (default: ER): ") or "ER")
+        strat = int(input("Strat # (default: 3): ") or "3")
+        payoff = int(input("Payoff # (default: 0): ") or "0")
+        saveRate = int(input("Saverate # (default: 1): ") or "1")
         
-        # # generate network
+        load_flag = int(input("Run(0) or Load(1) (default: 0): ") or "0")
+        
+        # Step 1. generate network
         nodes, adj = generate_random_network(model, num_nodes, p=0.2)
         # nodes, adj = facebook_clean()
         # nodes, adj = karate_clean()
-
         
-        # # run simulation
-        saveRate = 1
-        strat = 3
-        payoff = 0
-        simulation = Simulation(numIterations, saveRate, strat, payoff) # choose from 0, 1, 2
-        nodes_list, adj_list = simulation(nodes, adj)
         
-        # # visualize
+        # Step 2. run simulation
+        if load_flag:
+                nodes_list, adj_list, numIterations = load_data(strat, payoff, saveRate)              # Load pickles to save time
+        else:
+                simulation = Simulation(numIterations, saveRate, strat, payoff) # choose from 0, 1, 2
+                nodes_list, adj_list = simulation(nodes, adj)
         
-        visualize_list(nodes_list, adj_list, numIterations, "{0}+s{1}+p{2}".format(model, strat, payoff), True)
+        
+        print(f'time used to simulate: {time.perf_counter()-start}s')
+        
+        
+        
+        # Step 3. visualize
+        
+        start = time.perf_counter()
+        
+        visualize_list(nodes_list, adj_list, numIterations, "{0}+s{1}+p{2}".format(model, strat, payoff), pos_lock=True)
          # 4th parameter (model name) is for bookkeeping purposes
          # 5th parameter (defaulted to True) means position is LOCKED for future iteration
          # choose False to recalculate the position of Nodes every iteration (which significantly slows down the process)
-
-        end = time.perf_counter()
-        print(f'time elapsed: {end-start}')
+        
+        
+        print(f'time used to visualize: {time.perf_counter()-start}s \n')
 
         # complexity_graph()
