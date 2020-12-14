a22213a81278eeb3a778a87d47042b9bc0428a2d
Husky-Husker <59786467+Husky-Husker@users.noreply.github.com>
2020-12-13 19:44:24 -0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,6 +2,9 @@ import networkx as nx
 import numpy as np
 from cleaning import *
 import pandas as pd
+from networkx.algorithms.community import greedy_modularity_communities
+import os 
+
 
 def centrality(adj , node_dict, type):
 	G = nx.convert_matrix.from_numpy_matrix(adj)
@@ -22,13 +25,14 @@ def centrality(adj , node_dict, type):
 	return return_dict
 
 
-def all_measures( node_dict, adj, alpha = 0.9):
+def all_measures( node_dict, adj, iteration, alpha = 0.9):
 	G = nx.convert_matrix.from_numpy_matrix(adj)
-	dict_list = []
 
-	degree = nx.degree_centrality(G)
-	df = pd.DataFrame.from_dict(degree, orient = 'index')
-	df.columns = ['Degree']
+	df = pd.DataFrame.from_dict(node_dict, orient = 'index')
+	df.columns = ['Node']
+	df['iteration'] = iteration
+	df['status'] = pd.Series([val.status for val in node_dict.values()])
+	df['degree'] = pd.Series(nx.degree_centrality(G))
 	df['eigenvector'] = pd.Series(nx.eigenvector_centrality(G))
 	df['katz'] = pd.Series(nx.katz_centrality(G))
 	df['closeness'] = pd.Series(nx.closeness_centrality(G))
@@ -37,6 +41,33 @@ def all_measures( node_dict, adj, alpha = 0.9):
 	df['local_clustering_coefficients'] = pd.Series(nx.clustering(G))
 
 	return( df)
+
+def all_measures_master(node_dict_list, adj_list ):
+	master_df = pd.DataFrame()
+	it = 0
+	for node_dict, adj in zip(node_dict_list, adj_list):
+		values = all_measures(node_dict, adj, it, 0.9)
+		master_df = pd.concat([master_df, values])
+		it = it +1
+	if not os.path.exists('analysis'):
+		os.makedirs('analysis')
+	directory = os.path.dirname(__file__) + '/analysis' + '/centrality_values.csv'
+	master_df.to_csv(directory)
+	return master_df
+
+def community_detection(node_dict_list, adj_list ):
+	master_df = pd.DataFrame()
+	it = 0
+	for node_dict, adj in zip(node_dict_list, adj_list):
+		G = nx.from_numpy_matrix(adj)
+		c = list(greedy_modularity_communities(G))
+		print('iteration:' + str(it))
+		for val in c:
+			print(sorted(val))
+			print([node_dict.get(item,item).status for item in sorted(val)])
+			print()
+		it = it +1
+	return 
 
 
 def geodesic(adj):
