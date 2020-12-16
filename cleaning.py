d91195e4eee108564d87c7647af418a19c74505a
Husky-Husker <59786467+Husky-Husker@users.noreply.github.com>
2020-12-15 21:44:13 -0800

    Added facebook subgraph generation

--- cleaning.py
+++ cleaning.py
@@ -8,11 +8,11 @@ import random
 from simulations_updated import Node
 
 
-def generate_network(type, num_nodes, num_subgraphs = 100, cooperator_proportion= 0.5,  seed=100, p =0.5, m =3, k =3):
+def generate_network(type, num_nodes, num_subgraphs = 4, cooperator_proportion= 0.5,  seed=33, p =0.5, m =3, k =3):
 	if type == 'twitter':
 		return twitter_random_subgraph_clean(cooperator_proportion, num_subgraphs, seed)
 	elif type == 'facebook':
-		return facebook_clean(cooperator_proportion)
+		return facebook_random_subgraph_clean(cooperator_proportion, num_subgraphs, seed = seed )
 	elif type == 'karate':
 		return karate_clean(cooperator_proportion)
 	elif type == 'ER':
@@ -38,6 +38,39 @@ def twitter_random_subgraph_clean( cooperator_proportion= 0.5, num_subgraphs =10
 	subgraph_set = [ name + '.edges' for name in subgraph_set]
 	random.seed(seed)
 	subgraph_random_set = random.sample(subgraph_set, num_subgraphs)
+	combined = []
+	for subgraph in subgraph_random_set:
+		network = open(subgraph, 'r').read().splitlines()
+		combined.extend(network)
+
+	
+	sources = [int(L.split()[0]) for L in combined]
+	targets = [int(L.split()[1]) for L in combined]
+	G = nx.Graph()
+	E = len(sources)    # Number of edges
+	for i in range(E):
+		G.add_edge(sources[i], targets[i])
+
+	node_dict = generate_node_dict(G.number_of_nodes(), cooperator_proportion, 100 )
+
+	return node_dict, nx.to_numpy_array(G)
+
+
+def facebook_random_subgraph_clean( cooperator_proportion= 0.5, num_subgraphs =4, seed =33):
+	directory = os.path.dirname(__file__) + '/resources' + '/facebook'
+	os.chdir(directory)
+	subgraph_names = os.listdir(directory)
+	subgraph_set = set()
+	for name in subgraph_names:
+		subgraph_id = re.findall("^\d+", name)
+		subgraph_set.add(subgraph_id[0])
+
+	subgraph_set = list(subgraph_set)
+	subgraph_set = [ name + '.edges' for name in subgraph_set]
+	subgraph_set.sort()
+	random.seed(seed)
+	subgraph_random_set = random.sample(subgraph_set, num_subgraphs)
+	print(subgraph_random_set)
 	combined = []
 	for subgraph in subgraph_random_set:
 		network = open(subgraph, 'r').read().splitlines()
@@ -116,5 +149,7 @@ def generate_node_dict(num_nodes,  cooperator_proportion = 0.5, seed =100 ):
 			nodes[i] = Node(1, i)
 	return nodes
 
-	
+if __name__ == '__main__':
+	print(generate_network('facebook', 100)[0])
+
 
